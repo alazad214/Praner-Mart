42197f40cf0cae7aa64c8295b6ab163406dc0eaf
Al Azad <alazad214@gmail.com>
2024-05-17 00:20:50 +0600

    splash screen added

---
+++ lib/screens/splash.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:pranermart/screens/home_screen.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Get.offAll(const HomeScreen());
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return LayoutBuilder(builder: (context, constaints) {
+      return Scaffold(
+        body: SafeArea(
+            child: ListView(
+          children: [
+            Container(
+              height: screenSize.height / 1,
+              alignment: Alignment.center,
+              child: Center(
+                  child:
+                      Lottie.asset("assets/animations/anim.json", height: 100)),
+            )
+          ],
+        )),
+      );
+    });
+  }
+}
